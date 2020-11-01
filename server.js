bed6c9dbe783ad74f828a0b4b426c8f45a72d3bf
Rashed <irashad42@gmail.com>
2020-11-01 10:25:56 +0600

    add upload press static support

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use('/uploads', express.static('uploads'));
 
 app.use('/api/auth', auth);
 app.use('/api/users', users);

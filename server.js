f3638723b53cd2b138a805c4c845a6cd6a90919c
Rashed <irashad42@gmail.com>
2020-11-01 10:01:11 +0600

    rokstar_db to db

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
     startupDebugger(`Listening on port ${port}...`)
-    mongoose.connect(config.get('rokstar_db'), {
+    mongoose.connect(config.get('db'), {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
